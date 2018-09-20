55ce26a47f93ad3faef79dd4dac68f51cab19901
Yuefeng WANG <small.tortoise@hotmail.com>
2018-09-20 17:58:30 +0200

    Première fois : 20092018 par Yuefeng WANG

---
+++ app/src/main/java/com/smalltortoise/mamaisonconnect/activitys/ActivityBienvenue.java
@@ -0,0 +1,13 @@
+package com.smalltortoise.mamaisonconnect.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityBienvenue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenue);
+    }
+}
